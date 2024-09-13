c402c0a792ee13f56c97986edaf4ed06e2bd6b18
Seydou-nasser <seydounasser22@gmail.com>
2024-09-13 22:32:13 +0000

    cursor amelioration

--- 3DEUX1.IMMOBILIER.TG/Services/IUserService.cs
+++ 3DEUX1.IMMOBILIER.TG/Services/IUserService.cs
@@ -5,7 +5,7 @@ namespace _3DEUX1.IMMOBILIER.TG.Services
 {
     interface IUserService
     {
-        public Task<User> Login(string email, string password);
+        public Task<User?> Login(string email, string password);
         public Task<User?> RefrechLogin(string email);
         public Task<bool> Registre(RegisterModelSend model);
         public bool UserVerifier();
